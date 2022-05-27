d3bbbf924d90e8696d3c90f97eee04aa7618ee86
pro <quddbscjswow@naver.com>
2022-05-28 00:41:50 +0900

    자바 기초 클래스

---
+++ src/main/classtest/MyMathTest.java
@@ -0,0 +1,36 @@
+package main.classtest;
+
+public class MyMathTest {
+    public static void main(String[] args) {
+        MyMath mm = new MyMath();
+        long result1 = mm.add(5, 3);
+        long result2 = mm.subtract(5L, 3L);
+        long result3 = mm.multiply(5L, 3L);
+        double result4 = mm.divide(5L, 3L);
+
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+        System.out.println("result3 = " + result3);
+        System.out.println("result4 = " + result4);
+    }
+}
+
+class MyMath {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    long subtract(long a, long b) {
+        return a - b;
+    }
+
+    long multiply(long a, long b) {
+        return a * b;
+    }
+
+    //long 타입 으로 인자값을 넘겨줘도 divide 메소드에서의 매개변수는 double 타입으로 자동 형변환이 되어 값이 저장된다.
+    double divide(double a, double b) {
+        return a / b;
+    }
+
+}
