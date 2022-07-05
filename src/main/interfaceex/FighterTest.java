bd886045b5d6a75d7036261b6c353aa4c4b86e59
lby132 <quddbscjswow@naver.com>
2022-07-06 01:32:04 +0900

    자바 내부클래스

--- src/main/interfaceex/FighterTest.java
+++ src/main/interfaceex/FighterTest.java
@@ -4,7 +4,9 @@ public class FighterTest {
     public static void main(String[] args) {
         Fighter f = new Fighter();
 
-        if (f instanceof Unit){ System.out.println("f는 Unit 클래스의 자손입니다.");}
+        if (f instanceof Unit) {
+            System.out.println("f는 Unit 클래스의 자손입니다.");
+        }
         if (f instanceof Fightable) System.out.println("f는 Fightable 인터페이스를 구현했습니다.");
         if (f instanceof Movable) System.out.println("f는 Movable 인터페이스를 구현했습니다.");
         if (f instanceof Attackable) System.out.println("f는 Attackable 인터페이스를 구현했습니다.");
