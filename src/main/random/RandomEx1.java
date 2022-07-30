34025ed874a3dc7904c5ab9836403a07f3d4afa4
pro <quddbscjswow@naver.com>
2022-07-30 14:53:42 +0900

    java 기초 random

---
+++ src/main/random/RandomEx1.java
@@ -0,0 +1,21 @@
+package main.random;
+
+import java.util.Random;
+
+public class RandomEx1 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Random rand2 = new Random();
+
+        System.out.println("= rand =");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + ":" + rand.nextInt());
+        }
+
+        System.out.println();
+        System.out.println("= rand2 =");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + ":" + rand2.nextInt());
+        }
+    }
+}
