1abe80db729eb8f093f3f3ad690a084b30229a3b
lby132 <quddbscjswow@naver.com>
2022-08-21 23:57:15 +0900

    RandomAccessFile

---
+++ src/main/java2/inoutput/StandardIOEx2.java
@@ -0,0 +1,8 @@
+package main.java2.inoutput;
+
+public class StandardIOEx2 {
+    public static void main(String[] args) {
+        System.out.println("out : Hello World!");
+        System.err.println("err : Hello World!");   // err.println
+    }
+}
