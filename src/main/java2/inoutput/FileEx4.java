11f1e2bc5f73a0aa20152a495284bae9f0bd7b53
lby132 <quddbscjswow@naver.com>
2022-08-22 20:05:12 +0900

    File 입출력

---
+++ src/main/java2/inoutput/FileEx4.java
@@ -0,0 +1,33 @@
+package main.java2.inoutput;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FileEx4 {
+    public static void main(String[] args) {
+        String currDir = System.getProperty("user.dir");
+        File dir = new File(currDir);
+
+        File[] files = dir.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            File f = files[i];
+            final String name = f.getName();
+            final SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mma");
+            String attribute = "";
+            String size = "";
+
+            if (files[i].isDirectory()) {
+                attribute = "DIR";
+            } else {
+                size = f.length() + "";
+                attribute = f.canRead() ? "R" : " ";
+                attribute = f.canWrite() ? "W" : " ";
+                attribute = f.isHidden() ? "H" : " ";
+            }
+
+            System.out.printf("%s %3s %6s %s\n", df.format(new Date(f.lastModified())), attribute, size, name);
+        }
+    }
+}
